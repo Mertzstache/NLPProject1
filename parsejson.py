09526268b255b7280f275bcc25a1db6f75aec5e8
Eric Mertz <ericm338@Erics-MacBook-Pro-2.local>
2018-02-06 00:36:08 -0600

    added regex lib stuff

--- parsejson.py
+++ parsejson.py
@@ -10,6 +10,7 @@
 
 import sys
 import json
+import re
 #from pprint import pprint
 #import numpy as np
 #import os
@@ -22,8 +23,12 @@ def main():
     data = json.load(open(filename))
 
     for elem in data:
-        print(elem['text'])
-        input("Press Enter to continue...")
+        # print(elem['text'])
+        m = re.search('https?:\/\/(www\.)?', elem['text']) #'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+        if m:
+            m.group(0)
+            print(elem['text'])
+            input("Found a tweet with a link! Press Enter to continue...")
    # pprint(data)
 
 
