ac86167711798c070f8a59286705c90ce207c549
Eric Mertz <ericm338@dhcp-10-105-172-117.wireless.northwestern.private>
2018-02-11 15:47:30 -0600

    hi

--- main.py
+++ main.py
@@ -16,39 +16,39 @@ from wizard import Wizard
 
 
 def main():
-	"""main function"""
-	filename = str(sys.argv[1]) if len(sys.argv) > 1 else "gg2018.json"
-	gg_corpus = create_corpus_from_file(filename)
+    """main function"""
+    filename = str(sys.argv[1]) if len(sys.argv) > 1 else "gg2018.json"
+    gg_corpus = create_corpus_from_file(filename)
 
-	data = json.load(open(filename))
-	# wizard = Wizard(gg_corpus)
-	ggpage = wikipedia.page("Golden Globe Award")
-	award_list = ggpage.section("Motion picture awards")+ "\n"+ ggpage.section("Television awards")
+    data = json.load(open(filename))
+    wizard = Wizard(gg_corpus)
+    ggpage = wikipedia.page("Golden Globe Award")
+    award_list = ggpage.section("Motion picture awards")+ "\n"+ ggpage.section("Television awards")
 
-	print(wikipedia.summary("Call Me by Your Name (film)"))
+    print(wikipedia.summary("Call Me by Your Name (film)"))
 
-	ia = IMDb()
-	for person in ia.search_person('Mel Gibson'):
-    	print(person.personID, person['name'])
-	print(ia.search_person('Call Me by Your Name'))
-	# get the host for the awards
-	gg_host = wizard.get_host()
-	# print("host is:", gg_host)
+    ia = IMDb()
+    for person in ia.search_person('Mel Gibson'):
+        print(person.personID, person['name'])
+    print(ia.search_person('Call Me by Your Name'))
+    # get the host for the awards
+    gg_host = wizard.get_host()
+    # print("host is:", gg_host)
 
-	# gets the list of awards
-	award_list = wizard.get_award_names()
-	# print(award_list)
+    # gets the list of awards
+    award_list = wizard.get_award_names()
+    # print(award_list)
 
 
-	# gather info for each of the awards
-	wizard.get_all_award_info(award_list)
-	# for elem in data:
-	# 	# print(elem['text'])
-	# 	m = re.search('robbed', elem['text']) #'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
-	# 	if m:
-	# 		m.group(0)
-	# 		print(elem['text'])
-	# 		input("Found a tweet with a congrats! Press Enter to continue...")
+    # gather info for each of the awards
+    wizard.get_all_award_info(award_list)
+    # for elem in data:
+    #   # print(elem['text'])
+    #   m = re.search('robbed', elem['text']) #'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
+    #   if m:
+    #       m.group(0)
+    #       print(elem['text'])
+    #       input("Found a tweet with a congrats! Press Enter to continue...")
 
 
 
@@ -56,4 +56,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+    main()
